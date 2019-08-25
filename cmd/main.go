3a0b2ea713e7228eb560b332e13f40ba0d542197
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-08-25 21:07:37 +0800

    add flag

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"flag"
+)
 
+func main() {
+	var (
+		mode     string // "direct", "pma"(phpMyAdmin)
+		host     string
+		username string
+		password string
+		config   string
+	)
+	flag.StringVar(&mode, "m", "direct", "inject method")
+	flag.StringVar(&host, "h", "127.0.0.1:3306", "MySQL server address or phpMyAdmin url")
+	flag.StringVar(&username, "u", "root", "MySQL server username")
+	flag.StringVar(&password, "p", "", "MySQL server password")
+	flag.StringVar(&config, "c", "config.toml", "UDF config file path")
+	flag.Parse()
 }
